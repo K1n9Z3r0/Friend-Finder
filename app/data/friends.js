166b8bb43a4bf9131cd1999ccc76cf5200474be4
Zer0 <lucismcg@gmail.com>
2019-10-12 23:42:53 -0600

    uploading finished code

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friendMatch = [{
+        name: "Yogi Bear",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Yogi_Bear_Yogi_Bear.png/220px-Yogi_Bear_Yogi_Bear.png",
+        scores: ["2", "2", "1", "1", "2"]
+    }, {
+        name: "Droopy Dog",
+        photo: "https://upload.wikimedia.org/wikipedia/en/f/fd/Droopy_dog.png",
+        scores: ["2", "1", "1", "2", "1"]
+    }, {
+        name: "Wally Gator",
+        photo: "https://vignette.wikia.nocookie.net/hanna-barbera/images/e/e0/Wally_Gator.png/revision/latest?cb=20170712222715",
+        scores: ["1", "2", "1", "2", "1"]
+    }, {
+        name: "Grape Ape",
+        photo: "https://vignette.wikia.nocookie.net/hanna-barbera/images/4/44/Grape_Ape_In_LaL.png/revision/latest?cb=20190718075742",
+        scores: ["2", "2", "1", "2", "1"]
+    },
+
+];
+
+
+
+module.exports = friendMatch;
